cd45473b9107605152b87f95ffdab8af6dd6d229
Spencer Dent <mr.spencerdent@gmail.com>
2025-04-14 15:48:33 -0400

    feat: import other tests & start testing

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,26 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-
     eprintln!("{ast:#?}");
 
-    TokenStream::new()
+    let ident = &ast.ident;
+    let builder_ident = syn::Ident::new(&format!("{ident}Builder"), ident.span());
+
+    quote! {
+        struct #builder_ident {
+
+        }
+
+        impl #ident {
+            fn builder () -> #builder_ident {
+                #builder_ident {}
+            }
+        }
+    }
+    .into()
 }
