85256831ac581fb8392266402a91101b923d90b6
Spencer Dent <mr.spencerdent@gmail.com>
2025-05-16 08:43:20 -0400

    feat: begin work on BuilderError

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
-use std::error::Error;
+use std::{borrow::Cow, error::Error};
 
 use derive_builder::Builder;
+
+/// Occurs when the user either tries to incorrectly assign a field,
+/// or when they attempt to build the target struct while the builder
+/// is in an invalid state.
+#[derive(Clone, Debug, PartialEq)]
+pub enum BuilderError {
+    /// Typically occurs on the `build()` method. Examples include:
+    /// missing fields, constraint violations, and illogical structs.
+    InvalidState { message: Cow<'static, str> },
+    /// Typically occurs on the setter functions. Allows the builder
+    /// to catch problems before the user attempts to build the target.
+    InvalidField {
+        field_name: Cow<'static, str>,
+        message: Cow<'static, str>,
+    },
+}
+
+impl BuilderError {
+    pub fn missing_fields(fields: &[&str]) -> Self {
+        let missing_field_names = fields
+            .iter()
+            .map(|field_name| format!("`{field_name}`"))
+            .reduce(|acc, next| format!("{acc}, {next}"))
+            .unwrap_or_default();
+        Self::InvalidState {
+            message: format!("missing required fields: {missing_field_names}").into(),
+        }
+    }
+}
+
+impl std::fmt::Display for BuilderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BuilderError::InvalidState { message } => {
+                write!(f, "Unable to build #Target: {message}")
+            }
+            BuilderError::InvalidField {
+                field_name,
+                message,
+            } => write!(f, "Unable to assign field `{field_name}`: {message}"),
+        }
+    }
+}
+
+impl std::error::Error for BuilderError {}
 
 #[derive(Debug, Builder)]
 pub struct Command {
@@ -23,4 +68,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{command:#?}");
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn _missing_fields_formatted_correctly() {
+        let result = BuilderError::missing_fields(&["one", "two", "three"]);
+
+        assert_eq!(
+            result,
+            BuilderError::InvalidState {
+                message: "missing required fields: `one`, `two`, `three`".into()
+            }
+        )
+    }
 }
